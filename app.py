9c216a17d2f5de4032516487123a6832e9b886c1
Shane Adrian Lacsam <salacsam@ccc.edu.ph>
2023-01-08 10:41:06 +0800

    1st commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('home.html')
+
+
 
 
 if __name__ == '__main__':
